1fe6a0cdbf12fc2cc81172feb3d2b34a2d9a64c2
kofi a <kofi a@WHOREPHIC-AIR>
2016-12-07 22:25:08 -0700

    Created initial test solution

---
+++ TestWeatherApp/TestWeatherApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TestWeatherApp
+{
+    [Activity(Label = "TestWeatherApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
